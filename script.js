5546af351c1b18d7720cff1566ab600d4dd901c9
usericyber <user.icyberdev@gmail.com>
2025-09-17 18:01:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const PROXY_URL = 'https://corsproxy.io/?';
 const API_URL = "https://backend.craiyon.com/generate";
 
 const form = document.getElementById('image-form');
@@ -22,7 +22,7 @@ form.addEventListener('submit', async (e) => {
     generateBtn.disabled = true;
 
     try {
-        const response = await fetch(PROXY_URL + API_URL, {
+        const response = await fetch(PROXY_URL + encodeURIComponent(API_URL), {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
